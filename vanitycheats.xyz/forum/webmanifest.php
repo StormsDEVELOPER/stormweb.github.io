13f9ff20211f0bf7e903a69c6ac1eb17c59bcef5
StormsDEVELOPER <hyperr.ware@gmail.com>
2022-05-13 09:41:18 -0700

    BaseFiles
    These are the base files for StormDEV

---
+++ vanitycheats.xyz/forum/webmanifest.php
@@ -0,0 +1,12 @@
+{
+    "name": "VanityCheats",
+    "short_name": "Vanity",
+    "description": "VanityCheats. We offer reliable and undetected CS:GO Cheats for a low price. Your security is our priority.",
+    "lang": "en-US",
+    "dir": "LTR",
+    "display": "minimal-ui",
+    "scope": "\/forum\/",
+    "start_url": "\/forum\/?_pwa=1",
+    "background_color": "#151618",
+    "theme_color": "#720c0b"
+}
